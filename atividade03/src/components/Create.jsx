7f002b22de0df672ac91d84c4ba9f955820f1987
PauloFernandes <pgsfwow@hotmail.com>
2020-08-16 17:54:02 -0300

    Atividade03 e atividade 04
    update

--- atividade03/src/components/Create.jsx
+++ atividade03/src/components/Create.jsx
@@ -1,100 +1,69 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class Create extends Component {
-
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = { nome: '', curso: '', capacidade: '' };
 
-        this.state = { nome: '' ,curso: '', capacidade: ''}
-
-        this.setNome = this.setNome.bind(this)
-        this.setCurso= this.setCurso.bind(this)
-        this.setCapacidade= this.setCapacidade.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+        this.setNome = this.setNome.bind(this);
+        this.setCurso = this.setCurso.bind(this);
+        this.setCapacidade = this.setCapacidade.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-    setCurso(e){
 
-        this.setState({ curso: e.target.value})
-
-    }
     setNome(e) {
-        this.setState({ nome: e.target.value })
+        this.setState({ nome: e.target.value });
     }
-    setCapacidade(e){
 
-        this.setState({capacidade: e.target.value})
-
+    setCurso(e) {
+        this.setState({ curso: e.target.value });
     }
+
+    setCapacidade(e) {
+        this.setState({ capacidade: e.target.value });
+    }
+
     onSubmit(e) {
-        e.preventDefault()
-
-      /*  console.log(   'Nome: '    +  this.state.nome)
-        console.log(   'Curso: '    +  this.state.curso)
-        console.log(   'Capacidade: '    +  this.state.capacidade)*/
-
-        const novaDisciplina = {nome:this.state.nome,
-                                curso:this.state.curso,
-                                capacidade:this.state.capacidade}
-
-
-        axios.post('http://localhost:3000/disciplina',novaDisciplina)
-        .then(
-            (res)=>{
-                console.log("nova "+res.data.id+" disciplina cadastrada!")
-            }
-        )
-        .catch(
-            (error)=>{
-                console.log(error)
-            }
-        )
-
-
-        this.setState({ nome: '' })
-        this.setState({ curso:''})
-        this.setState({capacidade:''})
+        e.preventDefault();
+        const novaDisciplina = {nome:this.state.nome, curso:this.state.curso, capacidade:this.state.capacidade};
+        axios.post('http://localhost:3001/disciplinas', novaDisciplina)
+            .then(
+                (res)=>{
+                    console.log(res.data.id);
+                }
+            )
+            .catch(
+                (error)=>{
+                    console.log(error)
+                }
+            )
+        
+            this.setState({nome:'', curso:'', capacidade:''});
     }
 
     render() {
         return (
-
-            <div style={{ marginTop: 10 }}>     
-            
-                <h3>nome</h3>
-                {this.state.nome}
-                <br/>
-                <h3>curso</h3>
-                {this.state.curso}
-                <br/>
-                <h3>capacidade</h3>
-                {this.state.capacidade}
-
-                <h3>Criar Disciplina</h3>
+            <div style={{marginTop: 10}}>
+                <h3>Criar disciplina</h3>
                 <form onSubmit={this.onSubmit}>
-                    <div className='form-group'>
-                        <label>Nome:</label>
-                        <input type='text' className='form-control' value={this.state.nome} onChange={this.setNome} />
-
+                    <div className="form-group">
+                        <label>Nome: </label>
+                        <input type="text" className="form-control" value={this.state.nome} onChange={this.setNome}/>
                     </div>
-                    <div className='form-group'>
-                        <label>Curso:</label>
-                        <input type='text' className='form-control'value={this.state.curso} onChange={this.setCurso} />
-
+                    <div className="form-group">
+                        <label>Curso: </label>
+                        <input type="text" className="form-control" value={this.state.curso} onChange={this.setCurso}/>
                     </div>
-                    <div className='form-group'>
-                        <label>Capacidade:</label>
-                        <input type='text' className='form-control'value={this.state.capacidade} onChange={this.setCapacidade} />
-
+                    <div className="form-group">
+                        <label>Capacidade: </label>
+                        <input type="text" className="form-control" value={this.state.capacidade} onChange={this.setCapacidade}/>
                     </div>
-                    <div className='form-group'>
-
-                        <input type='submit' value='Criar' className='btn btn-primary' />
-
+                    <div className="form-group">
+                        <input type="submit" value="Criar" className="btn btn-primary"/>
                     </div>
                 </form>
             </div>
         )
-
     }
 }
