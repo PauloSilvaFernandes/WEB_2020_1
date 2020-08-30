539e2389f2ec176f3da7630baba18b5e1bfd27bb
PauloFernandes <pgsfwow@hotmail.com>
2020-08-30 13:15:57 -0300

    Revert "Atividade03 e atividade 04"
    This reverts commit 7f002b22de0df672ac91d84c4ba9f955820f1987.

--- atividade03/src/components/List.jsx
+++ atividade03/src/components/List.jsx
@@ -1,67 +1,13 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import TableRow from './TableRow';
+import React, { Component } from 'react'
 
-export default class List extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {disciplinas:[]}
-        this.apagarElementoPorId = this.apagarElementoPorId.bind(this);
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:3001/disciplinas')
-            .then(
-                (res) => {
-                    this.setState({disciplinas: res.data})
-                }
-            )
-            .catch(
-                (error) => {
-                    console.log(error)
-                }
-            )
-    }
-
-    montarTabela() {
-        if(!this.state.disciplinas) {
-            return;
-        }
-        return this.state.disciplinas.map(
-            (disciplina, i) => {
-                return <TableRow disciplina={disciplina} key={i} apagarElementoPorId={this.apagarElementoPorId}/>
-            }
-        )
-    }
-
-    apagarElementoPorId(id) {
-        let disciplinasTemp = this.state.disciplinas;
-        for (let i = 0; i < disciplinasTemp.length; i++) {
-            if(disciplinasTemp[i].id === id){
-                disciplinasTemp.slice(i,1)
-            }
-        }
-        this.setState({disciplinas:disciplinasTemp});
-    }
-
-    render() {
+export default class List extends Component{
+    render(){
         return(
+
             <div>
-                <table className="table table-striped" style={{marginTop: 20}}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Curso</th>
-                            <th>Capacidade</th>
-                            <th colSpan="2" style={{textAlign: "center"}}>Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.montarTabela()}
-                    </tbody>
-                </table>
+                <p>list</p>
             </div>
         )
+
     }
 }
